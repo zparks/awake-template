54a4189c9f6083fc0b07f53875342028354edbf8
Zak Parks <zak@parkstechnologies.com>
2021-06-27 03:34:51 -0400

    removed contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -6,69 +6,6 @@
         Submit the form below or email us at
         <a href="mailto: support@gymbet.app">support@gymbet.app</a>.
       </p>
-      <form name="contact" method="POST" data-netlify="true">
-        <!-- Name -->
-        <div class="field">
-          <label class="label is-medium">Name</label>
-          <div class="control has-icons-left">
-            <input
-              class="input is-medium"
-              type="text"
-              name="name"
-              placeholder="Your Name"
-            />
-            <span class="icon is-small is-left">
-              <font-awesome-icon icon="user" />
-            </span>
-          </div>
-        </div>
-        <!-- Email -->
-        <div class="field">
-          <label class="label is-medium">Email</label>
-          <div class="control has-icons-left">
-            <input
-              class="input is-medium"
-              type="email"
-              placeholder="you@email.com"
-              name="email"
-            />
-            <span class="icon is-small is-left">
-              <font-awesome-icon icon="envelope" />
-            </span>
-          </div>
-        </div>
-        <!-- Subject -->
-        <!-- <div class="field">
-        <label class="label is-medium">Subject</label>
-        <div class="control has-icons-left">
-          <input
-            class="input is-medium"
-            type="text"
-            placeholder="Brief Summary"
-          />
-          <span class="icon is-small is-left">
-            <font-awesome-icon icon="briefcase" />
-          </span>
-        </div>
-      </div> -->
-        <!-- Message -->
-        <div class="field">
-          <label class="label is-medium">Message</label>
-          <div class="control">
-            <textarea
-              class="textarea"
-              placeholder="Detailed description of your comment, request, or question"
-              rows="8"
-              name="message"
-            ></textarea>
-          </div>
-        </div>
-        <div class="control">
-          <button type="submit" class="button is-primary">
-            Contact Us
-          </button>
-        </div>
-      </form>
     </div>
   </div>
 </template>
